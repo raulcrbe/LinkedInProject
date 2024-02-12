9561dde3a5d521a6f6f1d5588e11570ef9aead05
raul <raulcrbe@gmail.com>
2024-02-12 20:53:20 +0100

    - Dockerized application - Added spotless for code formatting

--- sports/src/main/java/com/linkedin/sports/SportsApplication.java
+++ sports/src/main/java/com/linkedin/sports/SportsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SportsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SportsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SportsApplication.class, args);
+  }
 }
